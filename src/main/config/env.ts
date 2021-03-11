c8a1efc798d120db4dc3d2609e53a7644855b8eb
senugbin9850 <tanja1206@naver.com>
2021-03-11 14:43:38 +0900

    [ADD] export env config

---
+++ src/main/config/env.ts
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+
+config();
+
+export default {
+  port: Number(process.env.SERVER_PORT),
+  mysql: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  },
+  secretKey: process.env.JWT_SECRET,
+};
