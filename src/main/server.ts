5363ceb535598d2c3b2e57f74900d1d115a2125e
senugbin9850 <tanja1206@naver.com>
2021-03-14 22:56:24 +0900

    [FIX] import app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,9 @@
 import "module-alias/register";
 import "reflect-metadata";
 import env from "./config/env";
+import app from "./config/app";
 
 async function startServer() {
-  const app = (await import("./config/app")).default;
   app.listen(env.port, () => {
     console.log("server on!");
   });
