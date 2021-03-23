f85423a8c4882a93d839bf24652345c9436d6524
senugbin9850 <tanja1206@naver.com>
2021-03-23 22:06:10 +0900

    [ADD] create user

--- src/infra/db/repositories/user-repository.ts
+++ src/infra/db/repositories/user-repository.ts
@@ -1,13 +1,24 @@
+import { CreateUserRepository } from "@/data/protocols/db/repositories/create-user-repository";
 import { FindUserRepository } from "@/data/protocols/db/repositories/find-user-repository";
 import { User } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(User)
-export class UserRepository implements FindUserRepository {
+export class UserRepository
+  implements FindUserRepository, CreateUserRepository {
   public async findOne(id: string): Promise<User> {
     return await getRepository(User)
       .createQueryBuilder("user")
       .where("user.id = :id", { id })
       .getOne();
+  }
+
+  public async create(data: CreateUserRepository.Params): Promise<void> {
+    await getRepository(User)
+      .createQueryBuilder("user")
+      .insert()
+      .into(User)
+      .values(data)
+      .execute();
   }
 }
