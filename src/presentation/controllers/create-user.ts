104e0babefc6d376cde9d4f472e253293b6e8c82
senugbin9850 <tanja1206@naver.com>
2021-03-27 21:15:10 +0900

    [ADD] create user controller

---
+++ src/presentation/controllers/create-user.ts
@@ -0,0 +1,16 @@
+import { CreateUser } from "@/domain/usecase";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import { alreadyExistsData, ok } from "../helpers";
+
+export class CreateUserController implements Controller {
+  constructor(private readonly createUser: CreateUser) {}
+
+  async handle(request: CreateUser.Params): Promise<HttpResponse> {
+    try {
+      await this.createUser.create(request);
+    } catch (e) {
+      return alreadyExistsData();
+    }
+    return ok("success");
+  }
+}
